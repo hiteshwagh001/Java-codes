99707a81ef205c2a517cfef8d4738538ccae8147
hiteshwagh001 <111413855+hiteshwagh001@users.noreply.github.com>
2023-09-27 19:22:04 +0530

    Delete InterviewCodes directory

--- InterviewCodes/maxele.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class maxele{
-    public static void main(String[] args) {
-       
-	    int[] arr={23,231,53,1,44,24};
-	    Arrays.sort(arr);
-	    System.out.println(arr[0]);
-    }
-}
