9652272425c338fa0a5ff79a56ee75824a06d711
hiteshwagh001 <111413855+hiteshwagh001@users.noreply.github.com>
2023-09-28 18:09:37 +0530

    Add files via upload
    medium level codes

---
+++ mediumlevelcodesArray/findMissing.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+
+public class findMissing {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {3, 4, 5, 6, 7};
+
+        int[] missingElements = findMissingElements(array1, array2);
+
+        System.out.print("Missing elements in the second array: ");
+        for (int element : missingElements) {
+            System.out.print(element + " ");
+        }
+    }
+
+    public static int[] findMissingElements(int[] array1, int[] array2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : array1) {
+            set.add(num);
+        }
+
+        int[] missingElements = new int[array2.length];
+        int count = 0;
+
+        for (int num : array2) {
+            if (!set.contains(num)) {
+                missingElements[count++] = num;
+            }
+        }
+
+        int[] result = new int[count];
+        System.arraycopy(missingElements, 0, result, 0, count);
+
+        return result;
+    }
+}
